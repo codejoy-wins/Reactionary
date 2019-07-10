c991bb0e758858c94a708010c7fc91b12114fa2c
Max <mjann@ucsc.edu>
2019-07-10 15:06:24 -0700

    functional component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function MyFunc (){
+    return (<ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>)
+}
+
+ReactDOM.render(< MyFunc />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
