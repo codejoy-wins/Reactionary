da822591b2acf8b9ee3809fab05e83c64e37e03e
Max <mjann@ucsc.edu>
2019-07-10 15:25:48 -0700

    25 minutes in

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-
-function MyFunc (){
-    return (<ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>)
+function MyInfo(){
+    const MyStyle = {
+        color: "gold",
+    }
+    return (
+        <div>
+            <h1 style={MyStyle}>Max Jann</h1>
+            <p>I like coding and chess.  I like going to the beach and playing football and basketball.  My favorite shows are Game of Thrones, Stranger Things, and Ozark. Here are the top 3 places I want to visit.</p>
+            <ol>
+                <li>Maui</li>
+                <li>Jerusalem</li>
+                <li>Paris</li>
+            </ol>
+        </div>
+    )
 }
 
-ReactDOM.render(< MyFunc />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(< MyInfo />,document.getElementById("root"));
